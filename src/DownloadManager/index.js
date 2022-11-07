56b0edeb5850862a7bbba8fa60f6ba7a489e66e8
nacellius <15840839+nacellius@users.noreply.github.com>
2022-11-07 15:37:06 -0500

    implemented main checkbox functionality

--- src/DownloadManager/index.js
+++ src/DownloadManager/index.js
@@ -1,4 +1,4 @@
-import React, {createRef, useState} from "react";
+import React, {createRef, useEffect, useState} from "react";
 import "./style.css";
 import downloadIcon from "../assets/download.svg";
 
@@ -13,17 +13,19 @@ const CHECKBOX_STATES = {
     indeterminate: "indeterminate",
 }
 
+
 function DownloadManager({ data }) {
-    // Load given download item data into a state
+    // Initialize needed states
     const [downloadItems, setDownloadItems] = useState(data);
-    const [selectedNumberTotal, setselectedNumberTotal] = useState(0);
+    const [selectedTotal, setSelectedTotal] = useState(0);
     const [mainCheckboxState, setMainCheckboxState] = useState(CHECKBOX_STATES.unchecked);
 
     const mainCheckbox = createRef();
 
+
     // Update download item's "selected" value when table item is clicked
     const handleTableRowClick = (index) => {
-        // Disallow selecting of unavailable items
+        // Disallow selection of non-available items
         if (downloadItems[index].status !== ITEM_STATUS.available) {
             return null;
         }
@@ -38,9 +40,9 @@ function DownloadManager({ data }) {
 
         // Update number of total selected items
         if (newSelectedValue) {
-            setselectedNumberTotal(selectedNumberTotal + 1);
+            setSelectedTotal(selectedTotal + 1);
         } else {
-            setselectedNumberTotal(selectedNumberTotal - 1);
+            setSelectedTotal(selectedTotal - 1);
         }
 
         setDownloadItems(newDownloadItems);
@@ -51,16 +53,63 @@ function DownloadManager({ data }) {
         let selectedItems = "Files selected for download: \n";
         downloadItems.forEach(item => {
             if (item.selected) {
-                selectedItems = selectedItems.concat(`• Device: ${item.device}, Path: ${item.path} \n`)
+                selectedItems = selectedItems.concat(`• Device: ${item.device}, Path: ${item.path} \n`);
             }
         });
         alert(selectedItems);
     }
 
+    const handleMainCheckboxChange = () => {
+        let newDownloadItems = [];
+        let newselectedTotal = 0;
+
+        if (mainCheckboxState !== CHECKBOX_STATES.checked) {
+            // De-select all items if all are selected
+            downloadItems.forEach(item => {
+                // only allow selecting available items 
+                if (item.status === ITEM_STATUS.available) {
+                    newDownloadItems.push({...item, selected: true});
+                    newselectedTotal++;
+                } else {
+                    newDownloadItems.push(item);
+                }
+            });
+        } else {
+            // Select all items if none are selected, or some are selected
+            downloadItems.forEach(item => {
+                newDownloadItems.push({...item, selected: false});
+            });
+        }
+
+        setSelectedTotal(newselectedTotal);
+        setDownloadItems(newDownloadItems);
+    }
+
+    // Update the main checkbox's "checked" and "indeterminate" properties whenever
+    // an item is selected/unselected
+    useEffect(() => {
+        if (selectedTotal === downloadItems.length) {
+            setMainCheckboxState(CHECKBOX_STATES.checked);
+            mainCheckbox.current.checked = true;
+            mainCheckbox.current.indeterminate = false;
+        } else if (selectedTotal === 0) {
+            setMainCheckboxState(CHECKBOX_STATES.unchecked);
+            mainCheckbox.current.checked = false;
+            mainCheckbox.current.indeterminate = false;
+        } else {
+            setMainCheckboxState(CHECKBOX_STATES.indeterminate);
+            mainCheckbox.current.checked = false;
+            mainCheckbox.current.indeterminate = true;
+        }
+    }, [selectedTotal, downloadItems, mainCheckbox]);
+
+
     // Create a table item for each download item in the data
     const downloadsTableItems = downloadItems.map( (item, index) => {
         const availability = item.status === ITEM_STATUS.available;
         const selected = item.selected ?? false;
+        // TODO: consider using i18n here instead
+        const formattedStatus = item.status[0].toUpperCase() + item.status.slice(1).toLowerCase();
         return (
             <tr
                 className={`downloadTableItem ${selected && "availableItem"}`}
@@ -79,7 +128,7 @@ function DownloadManager({ data }) {
                         {availability ? <div className="availabilityIndicator" /> : null}
                     </div>
                 </td>
-                <td className="itemStatusColumn">{item.status}</td>
+                <td className="itemStatusColumn">{formattedStatus}</td>
             </tr>
         )
     })
@@ -89,11 +138,14 @@ function DownloadManager({ data }) {
         <div className="downloadManager" >
             <div className="toolBar">
                 <label className="mainCheckbox">
-                    <input ref={mainCheckbox} type="checkbox"/>
+                    <input
+                        ref={mainCheckbox}
+                        type="checkbox"
+                        onChange={() => handleMainCheckboxChange()}/>
                     <span className="mainCheckboxText">
                         {
-                            selectedNumberTotal > 0
-                                ? `Selected ${selectedNumberTotal}` 
+                            selectedTotal > 0
+                                ? `Selected ${selectedTotal}` 
                                 : "None Selected"
                         }
                     </span>
